ccd2480e797a0ac1ab7e1209fc6218c5a0bbfda9
v3rg1l38 <vergil@localhost.localdomain>
2020-03-04 20:03:51 +0100

    ...

--- config/logger.js
+++ config/logger.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const globals = require('./Globals');
 
-class Logger {
-    constructor() {}
-
-    static loginLog(log) {
-        log += '\n';
-        fs.appendFile(globals.loginLogs, log, 'utf-8', (err) => {
-            if(err)
-                console.log(`[Error:loginLog]: ${err}`);
-        });
-    }
-
-    static serverLog(log) {
-        log += '\n';
-        fs.appendFile(globals.serverLogs, log, 'utf-8', (err) => {
-            if(err)
-                console.log(`[Error:serverLog]: ${err}`);
-        });
-    }
+const loginLog = (log) => {
+    log += '\n';
+    fs.appendFile(globals.loginLogs, log, 'utf-8', (err) => {
+        if(err)
+            console.log(`[Error:loginLog]: ${err}`);
+    });
 }
 
-module.exports = Logger;
+const serverLog = (log) => {
+    log += '\n';
+    fs.appendFile(globals.serverLogs, log, 'utf-8', (err) => {
+        if(err)
+            console.log(`[Error:serverLog]: ${err}`);
+    });
+}
+
+
+module.exports = {
+    loginLog,
+    serverLog
+}
