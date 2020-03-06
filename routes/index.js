fa83386adc4f510d2dac6f38d979a59242f26ebb
v3rg1l38 <vergil@localhost.localdomain>
2020-03-06 21:47:31 +0100

    ...

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/db');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const resp = await db.sendQuery('SELECT * FROM Posts');
+        return res.json({ status: 200, data: resp });
+    }
+    catch(err) {
+        console.log(`ERROR: ${err}`);
+        res.json({ status: 500, message: `ERROR: ${err}` });
+    }
+});
+
+
+module.exports = router;
