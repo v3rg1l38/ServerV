eb3c0a1d7493de44c4775f12eb7fc482196f118f
v3rg1l38 <v3rg1l38@users.noreply.github.com>
2020-03-07 19:22:35 +0100

    ...

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const db = require('../config/db');
 
 
+const pk = async () => {
+    const resp = await db.sendQuery('SELECT id FROM Posts');
+    return resp;
+}
+
 router.get('/', async (req, res) => {
     try {
-        const resp = await db.sendQuery('SELECT id, Title FROM Posts');
+        // const resp = await db.sendQuery('SELECT id, Title FROM Posts');
+        const rsp = pk();
         return res.json({ status: 200, data: resp });
     }
     catch(err) {
